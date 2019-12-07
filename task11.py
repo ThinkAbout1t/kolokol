18c57af0c5d3810111a1e3ead83710b83ed06255
ThinkAbout1t <58637911+ThinkAbout1t@users.noreply.github.com>
2019-12-07 23:06:02 +0200

    Update task11.py

--- task11.py
+++ task11.py
@@ -1,1 +1,18 @@
-asd
+'''Дано натуральне n, m. Отримати суму m останніх цифр числа n
+(використовувати рядковий тип даних і методи по роботі з рядками забороняється).
+Pakhomov Olexandr 122Б
+'''
+while True:
+    try:
+        n=int(input('n='))
+        m=int(input('m='))
+        summa=0
+        while m!=0:
+            n_last=n%10
+            n//=10
+            m-=1
+            summa+=n_last
+        print(summa)
+        break
+    except ValueError:
+        print('input int')
